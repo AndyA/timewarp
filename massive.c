bd45d09bd2d80d896e5744a8759f1fe1b5c60059
Andy Armstrong <andy@hexten.net>
2013-05-22 21:29:47 +0100

    Fwd/inv/

--- massive.c
+++ massive.c
@@ -28,7 +28,8 @@ typedef struct {
     int disabled;
     double mass;
     double drag;
-    double rms_weight;
+    double rms_fwd_weight;
+    double rms_inv_weight;
     average average;
     average rms_acc;
   } plane[Y4M2_N_PLANE];
@@ -55,10 +56,8 @@ static void massive__setup(y4m2_frame *frame,
 
   for (unsigned p = 0; p < Y4M2_N_PLANE; p++) {
     for (unsigned i = 0; i < frame->i.plane[p].size; i++) {
-      dp->position = *fp++;
-      dp->velocity = 0;
-      dp->average = 0;
-      dp->rms = 0;
+      dp->position = dp->rms = *fp++;
+      dp->velocity = dp->average =  0;
     }
   }
 }
@@ -79,16 +78,23 @@ static void massive__frame(y4m2_frame *frame,
         double max = (p == Y4M2_Y_PLANE) ? 235 : 255;
         double drag = wrk->plane[p].drag;
         double mass = wrk->plane[p].mass;
-        int do_rms = wrk->plane[p].rms_weight != 0;
+        double fwd_weight = wrk->plane[p].rms_fwd_weight;
+        double inv_weight = wrk->plane[p].rms_inv_weight;
+        int do_rms = fwd_weight != 0 || inv_weight != 1;
+
         for (unsigned i = 0; i < frame->i.plane[p].size; i++) {
           double local_mass = mass;
+
           if (do_rms) {
             dp->average = average_next(&wrk->plane[p].average, dp->average, *fp);
             double dy = *fp - dp->average;
-            dp->rms = average_next(&wrk->plane[p].rms_acc, dp->rms, dy * dy);
-            local_mass = mass + wrk->plane[p].rms_weight * sqrt(dp->rms);
+            double rms = sqrt(dp->rms = average_next(
+                                          &wrk->plane[p].rms_acc, dp->rms, dy * dy));
+            if (rms < SIGMA) rms = SIGMA;
+            local_mass = mass + (fwd_weight * rms) + (inv_weight / rms);
             if (local_mass < SIGMA) local_mass = SIGMA;
           }
+
           double force = (*fp - dp->position) - (dp->velocity * fabs(dp->velocity) * drag);
           dp->velocity += force / local_mass;
 
@@ -99,6 +105,7 @@ static void massive__frame(y4m2_frame *frame,
           fp++;
           dp++;
         }
+
         if (do_rms) {
           average_update(&wrk->plane[p].average);
           average_update(&wrk->plane[p].rms_acc);
@@ -146,7 +153,8 @@ y4m2_output *massive_hook(y4m2_output *out, jd_var *opt) {
     wrk->plane[p].mass = util_get_real(jd_rv(slot, "$.mass"), 1);
     wrk->plane[p].drag = util_get_real(jd_rv(slot, "$.drag"), 0);
 
-    wrk->plane[p].rms_weight = util_get_real(jd_rv(slot, "$.rms_weight"), 0);
+    wrk->plane[p].rms_fwd_weight = util_get_real(jd_rv(slot, "$.rms_fwd_weight"), 0);
+    wrk->plane[p].rms_inv_weight = util_get_real(jd_rv(slot, "$.rms_inv_weight"), 1);
     average_config(&wrk->plane[p].average, slot, "rms");
     average_config(&wrk->plane[p].rms_acc, slot, "rms");
   }

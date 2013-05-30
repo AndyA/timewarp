bc4770cc649cfa7e86c7af36a3a0c8f90bcb6f2e
Andy Armstrong <andy@hexten.net>
2013-05-30 22:53:00 +0100

    Tweaked.

--- misc/wibble.sh
+++ misc/wibble.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 rate=24
+url=rtmp://localhost/live/c920
+
 v4l2-ctl --set-fmt-video=width=1280,height=720,pixelformat=1
 v4l2-ctl --set-parm=$rate
 ffmpeg -y \
   -an -f h264 -i <( v4l2h264 -o ) \
   -pix_fmt yuv420p \
-  -t 60 \
   -f yuv4mpegpipe - | ./timewarp config/wibble.json | \
   ffmpeg -y -f yuv4mpegpipe -i - -an \
-    -c:v libx264 -b:v 3000k -preset veryfast c920.ts \
+    -c:v libx264 -b:v 3000k -preset veryfast -profile:v baseline \
+    -f flv "$url"
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
